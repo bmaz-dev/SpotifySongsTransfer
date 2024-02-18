504b64222977edfe4e9c0f8c0b27440e848b83a8
bmaz.dev <bmaz.dev@gmail.com>
2024-02-18 19:11:01 +0100

    Playlist-list component declaration

---
+++ src/app/components/playlist-list/playlist-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-playlist-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './playlist-list.component.html',
+  styleUrls: ['./playlist-list.component.scss']
+})
+export class PlaylistListComponent {
+
+}
